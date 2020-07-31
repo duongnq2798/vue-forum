e0b544aff85d171841bcca6c98eb2c953f5fc1d3
duongnq2798 <duongnq27.98@gmail.com>
2020-07-31 15:01:02 +0700

    Organize threads in forums

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -15,7 +15,6 @@ export default {
   components: {
     ThreadListItem
   },
-
   props: {
     threads: {
       required: true,
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import PageHome from '@/pages/PageHome'
 import PageThreadShow from '@/pages/PageThreadShow'
+import Forum from '@/pages/PageForum'
 import NotFound from '@/pages/PageNotFound'
 
 Vue.use(Router)
@@ -12,6 +13,12 @@ export default new Router({
       path: '/',
       name: 'PageHome',
       component: PageHome
+    },
+    {
+      path: '/forum/:id',
+      name: 'Forum',
+      component: Forum,
+      props: true
     },
     {
       path: '/thread/show/:id',
